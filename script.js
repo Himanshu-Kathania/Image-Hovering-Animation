f5b0206ea493fea7a80ecb532d4b9e2d91b00855
Himanshu Kathania <76757042+Himanshu-Kathania@users.noreply.github.com>
2023-11-07 16:43:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseenter", function () {
+    val.childNodes[3].style.opacity = 1;
+  });
+});
+
+elem.forEach(function (val) {
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
+  });
+
+  val.addEventListener("mousemove", function (details) {
+    val.childNodes[3].style.left = details.x + "px";
+    // val.childNodes[3].style.top = details.y + "px";
+  });
+});
